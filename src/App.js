a009bf299cb396fec55999cd334a15090c57ba1b
Justin Zhu <justin.zhu@sjsu.edu>
2020-08-28 09:54:58 -0700

    Fully functioning add event and buttons

--- src/App.js
+++ src/App.js
@@ -2,136 +2,122 @@ import React, {Component} from 'react';
 import {CLIENT_ID, API_KEY, DISCOVERY_DOCS, SCOPES} from './config.json';
 import './App.css';
 
+// LINKS: https://developers.google.com/calendar/quickstart/js
+// https://developers.google.com/identity/sign-in/web/listeners
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      signedIn: '',
       name: '',
+      isSignedIn: null,
     };
   }
   componentDidMount() {
-    console.log('Poggers');
-    var auth_button = document.getElementById('authorize_button');
-    // auth_button.style = {display: 'block'};
-    auth_button.style.display = 'block';
+    window.gapi.load('client:auth2', () => {
+      this.initializeGapi();
+    });
   }
 
-  handleSignIn = async () => {
-    var authorizeButton = document.getElementById('authorize_button');
-    var gapi = await window.gapi;
-    gapi.load('auth2', () => {
-      let auth2 = gapi.auth2.init({
+  initializeGapi = () => {
+    window.gapi.client
+      .init({
         apiKey: API_KEY,
         clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
+        discoveryDocs: [DISCOVERY_DOCS],
         scope: SCOPES,
+      })
+      .then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        console.log('Auth: ', this.auth.isSignedIn.get());
+        console.log('Profile: ', this.auth.currentUser.get());
+
+        this.handleAuthChange();
+        this.auth.isSignedIn.listen(this.handleAuthChange);
       });
-      if (this.state.signedIn === '') {
-        let signedIn = auth2.isSignedIn.get();
-        this.setState({signedIn});
-      }
-      // this.consoleLogState();
-      // console.log(auth2.isSignedIn.get());
+  };
 
-      if (this.state.signedIn === false) {
-        Promise.resolve(
-          auth2.signIn().then(
-            (val) => {
-              console.log('Signed in?', val);
-              this.setState({name: val.rt.Ad});
-              console.log(val.rt.Ad);
-              this.postSignIn();
-            },
-            (error) => {
-              this.setState({signedIn: false});
-              console.log('error, popup closed');
-            }
-          )
-        );
-      }
-      // auth2.isSignedIn.listen(this.postSignIn);
+  handleAuthChange = () => {
+    if (this.auth.isSignedIn.get()) {
+      this.setState({name: this.auth.currentUser.get().rt.Ad});
+      document.getElementById('name-div').style.display = 'block';
+    }
+    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+  };
 
-      // .then((authResult) => {
-      //   var auth2 = gapi.auth2.getAuthInstance();
-      //   auth2.isSignedIn.listen(this.postSignIn());
-      //   auth2.signIn();
-      //   auth2.isSignedIn.listen(this.postSignIn());
-      // });
+  handleAddEvent = () => {
+    console.log('Add event!');
+    var event = {
+      summary: 'Google I/O 2015',
+      location: '800 Howard St., San Francisco, CA 94103',
+      description: "A chance to hear more about Google's developer products.",
+      start: {
+        dateTime: '2020-08-28T09:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      end: {
+        dateTime: '2020-08-28T17:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      recurrence: ['RRULE:FREQ=DAILY;COUNT=1'],
+      attendees: [
+        // {email: 'lpage@example.com'}, {email: 'sbrin@example.com'}
+      ],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          {method: 'email', minutes: 24 * 60},
+          {method: 'popup', minutes: 10},
+        ],
+      },
+    };
+
+    var request = window.gapi.client.calendar.events.insert({
+      calendarId: 'primary',
+      resource: event,
+    });
+
+    request.execute((event) => {
+      // console.log(event.htmlLink);
+      window.open(event.htmlLink);
     });
   };
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <div>
+          <button onClick={this.handleAddEvent}>Add Event</button>
+          <button onClick={this.handleSignOut}>Sign Out</button>
+        </div>
+      );
+    } else {
+      return <button onClick={this.handleSignIn}>Sign In</button>;
+    }
+  }
+
   handleSignOut = () => {
-    // window.gapi.signOut();
-    console.log('signOut');
-    // var gapi = window.gapi;
-    window.gapi.auth2
-      .getAuthInstance()
-      .signOut()
-      .then(function () {
-        console.log(
-          'After sign out',
-          window.gapi.auth2.getAuthInstance().isSignedIn.get()
-        );
-        document.getElementById('authorize_button').style.display = 'block';
-        document.getElementById('signOut_button').style.display = 'none';
-      });
+    this.auth.signOut();
     this.setState({name: ''});
   };
 
-  consoleLogState = () => {
-    console.log(this.state.signedIn);
-  };
-
-  postSignIn = () => {
-    document.getElementById('authorize_button').style.display = 'none';
-    document.getElementById('signOut_button').style.display = 'block';
-  };
-
-  authorize = (gapi) => {
-    gapi.auth2.init({}).then(function () {
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.isSignedIn.listen(postSignIn);
-      auth2.signIn();
-
-      var postSignIn = function (val) {
-        console.log('signed in');
-        gapi.client.load('calendar', 'v3', () => {
-          console.log('calendar');
-        });
-      };
-
-      // this.updateSigninStatus(gapi.auth2.getAuthInstance.isSignedIn.get());
-    });
-
-    // gapi.auth2.getAuthInstance().signIn().then(this.updateSignInButtons());
-  };
-  updateSignInButtons = () => {
-    console.log('SignIn Buttons!');
-    document.getElementById('authorize_button').style.display = 'none';
-    document.getElementById('signOut_button').style.display = 'block';
+  handleSignIn = () => {
+    this.auth.signIn();
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello World</h1>
-          <h2>Name: {this.state.name}</h2>
-          <button
-            id="authorize_button"
-            onClick={this.handleSignIn}
-            style={{display: 'none'}}
-          >
-            Sign in
-          </button>
-          <button
-            id="signOut_button"
-            onClick={this.handleSignOut}
-            style={{display: 'none'}}
-          >
-            Sign Out
-          </button>
+          <h1>Welcome to Event Adder</h1>
+          <div id="name-div" style={{display: 'block'}}>
+            {' '}
+            <h2> {this.state.name}</h2>
+          </div>
+
+          <div>{this.renderAuthButton()}</div>
         </header>
       </div>
     );

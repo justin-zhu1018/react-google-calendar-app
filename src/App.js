a15d98cb4074053b45ad74e64b1ccf70b57b37eb
Justin Zhu <justin.zhu@sjsu.edu>
2020-08-27 22:15:08 -0700

    Google sign in complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CLIENT_ID, API_KEY, DISCOVERY_DOCS, SCOPES} from './config.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedIn: '',
+    };
+  }
+  componentDidMount() {
+    console.log('Poggers');
+    var auth_button = document.getElementById('authorize_button');
+    // auth_button.style = {display: 'block'};
+    auth_button.style.display = 'block';
+  }
 
-export default App;
+  handleSignIn = async () => {
+    var authorizeButton = document.getElementById('authorize_button');
+    var gapi = await window.gapi;
+    gapi.load('auth2', () => {
+      let auth2 = gapi.auth2.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+      if (this.state.signedIn === '') {
+        let signedIn = auth2.isSignedIn.get();
+        this.setState({signedIn});
+      }
+      // this.consoleLogState();
+      // console.log(auth2.isSignedIn.get());
+
+      if (this.state.signedIn === false) {
+        Promise.resolve(
+          auth2.signIn().then(
+            (val) => {
+              console.log('Signed in?', val);
+              this.postSignIn();
+            },
+            (error) => {
+              this.setState({signedIn: false});
+              console.log('error, popup closed');
+            }
+          )
+        );
+      }
+      // auth2.isSignedIn.listen(this.postSignIn);
+
+      // .then((authResult) => {
+      //   var auth2 = gapi.auth2.getAuthInstance();
+      //   auth2.isSignedIn.listen(this.postSignIn());
+      //   auth2.signIn();
+      //   auth2.isSignedIn.listen(this.postSignIn());
+      // });
+    });
+  };
+
+  handleSignOut = () => {
+    // window.gapi.signOut();
+    console.log('signOut');
+  };
+
+  consoleLogState = () => {
+    console.log(this.state.signedIn);
+  };
+
+  postSignIn = () => {
+    document.getElementById('authorize_button').style.display = 'none';
+    document.getElementById('signOut_button').style.display = 'block';
+  };
+
+  authorize = (gapi) => {
+    gapi.auth2.init({}).then(function () {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.isSignedIn.listen(postSignIn);
+      auth2.signIn();
+
+      var postSignIn = function (val) {
+        console.log('signed in');
+        gapi.client.load('calendar', 'v3', () => {
+          console.log('calendar');
+        });
+      };
+
+      // this.updateSigninStatus(gapi.auth2.getAuthInstance.isSignedIn.get());
+    });
+
+    // gapi.auth2.getAuthInstance().signIn().then(this.updateSignInButtons());
+  };
+  updateSignInButtons = () => {
+    console.log('SignIn Buttons!');
+    document.getElementById('authorize_button').style.display = 'none';
+    document.getElementById('signOut_button').style.display = 'block';
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World</h1>
+          <button
+            id="authorize_button"
+            onClick={this.handleSignIn}
+            style={{display: 'none'}}
+          >
+            Sign in
+          </button>
+          <button
+            id="signOut_button"
+            onClick={this.handleSignOut}
+            style={{display: 'none'}}
+          >
+            Sign Out
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
